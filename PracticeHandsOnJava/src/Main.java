229db4291dab037ca4698ca900978170289fcabf
Alec Fortin <alec.fortin@outlook.com>
2021-02-19 21:54:40 -0500

    add input from file

--- PracticeHandsOnJava/src/Main.java
+++ PracticeHandsOnJava/src/Main.java
@@ -1,21 +1,24 @@
-import java.sql.SQLOutput;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Enter your name: ");
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("students.txt"));
 
-        if(s.equals("Alec")) {
-            System.out.println("Hey Alec");
-        } else if(s.equals("Sally")){
-            System.out.println("Hey Sally");
-        } else{
-            System.out.println("You're not welcome here");
+        List<String> students = new ArrayList<String>();
+
+        while(in.hasNextLine()){
+            students.add(in.nextLine());
+        }
+        for(int i = 0; i < students.size(); i++){
+            System.out.println("Name: " + students.get(i));
         }
 
         in.close();
+
     }
 }
