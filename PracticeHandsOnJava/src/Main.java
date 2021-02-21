94b5f8aaed6a8aa0135b8845180c13b2016a3261
Alec Fortin <alec.fortin@outlook.com>
2021-02-20 20:06:21 -0500

    iterators

--- PracticeHandsOnJava/src/Main.java
+++ PracticeHandsOnJava/src/Main.java
@@ -1,21 +1,37 @@
+import javax.sound.midi.Soundbank;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-     
-
         LinkedList<String> names = new LinkedList<String>();
         names.push("alec");
         names.push("emily");
         names.push("louise");
-        System.out.println(names.pop());
-        System.out.println(names.pop());
-        System.out.println(names.pop());
+
+        Iterator<String> it = names.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        names.add(2, "Bean");
+
+        for(String s: names){
+            System.out.println(s);
+        }
+
+        ListIterator<String> lit = names.listIterator();
+        lit.next();
+        lit.next();
+        lit.add("Kuiper");
+
+        for(String s: names){
+            System.out.println(s);
+        }
+
+
     }
 }
