c81cca889229f2835bbdb97341a632a747227e11
Alec Fortin <alec.fortin@outlook.com>
2021-02-20 20:53:12 -0500

    working with 2D arrays

--- PracticeHandsOnJava/src/Main.java
+++ PracticeHandsOnJava/src/Main.java
@@ -1,39 +1,19 @@
-import javax.sound.midi.Soundbank;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList<String> names = new LinkedList<String>();
-        names.push("alec");
-        names.push("emily");
-        names.push("louise");
+        int[][] data = {
+                {4,6,3,10},
+                {4,2,40,1},
+                {5,34,1,43}
+        };
 
-        Iterator<String> it = names.iterator();
+        //System.out.println(data[1][2]);
 
-        while(it.hasNext()){
-            System.out.println(it.next());
+        for(int i = 0; i < data.length; i++){
+            for(int j = 0; j < data[i].length; j++){
+                System.out.print(data[i][j]+ " ");
+            }
+            System.out.println();
         }
-
-        names.add(2, "Bean");
-
-        for(String s: names){
-            System.out.println(s);
-        }
-
-        ListIterator<String> lit = names.listIterator();
-        lit.next();
-        lit.next();
-        lit.add("Kuiper");
-
-        for(String s: names){
-            System.out.println(s);
-        }
-
-        System.out.println("Hi Louise");
-
-
     }
 }
