9dd032f8485c80bf426c5464acbd71987c291364
Alec Fortin <alec.fortin@outlook.com>
2021-02-21 22:16:01 -0500

    maze movements

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -4,7 +4,7 @@ public class MazeSolver {
 
     static int[][] maze = {
             {2,0,1,1},
-            {1,1,0,1},
+            {1,1,1,0},
             {0,1,1,1}
     };
     //0 = wall
@@ -15,10 +15,56 @@ public class MazeSolver {
 
 
     public static void main(String[] args) {
-        Position p = new Position(3, 0);
+        Position p = new Position(0,3);
         path.push(p);
 
-        maze[path.peek().y][path.peek().x] = 0;
+
+        while(true){
+            int y = path.peek().y;
+            int x = path.peek().x;
+
+            maze[y][x] = 0;
+
+            //down
+            if(maze[y+1][x] == 2){
+                System.out.println("Moved down \nYou won!");
+            }else if(maze[y+1][x] == 1){
+                System.out.println("Moved down");
+                path.push(new Position(y+1, x));
+                continue;
+            }
+
+            //left
+            if(maze[y][x-1] == 2){
+                System.out.println("Moved left\nYou won!");
+            }else if(maze[y][x-1] == 1){
+                System.out.println("Moved left");
+                path.push(new Position(y, x-1));
+                continue;
+            }
+
+            //up
+            if(maze[y-1][x] == 2){
+                System.out.println("Moved up\nYou won!");
+            }else if(maze[y-1][x] == 1){
+                System.out.println("Moved up");
+                path.push(new Position(y-1, x));
+                continue;
+            }
+            //right
+            if(maze[y][x+1] == 2){
+                System.out.println("Moved right\nYou won!");
+            }else if(maze[y][x+1] == 1){
+                System.out.println("Moved right");
+                path.push(new Position(y, x+1));
+                continue;
+            }
+            path.pop();
+            if(path.size() < 0){
+                System.out.println("No path");
+            }
+        }
+
 
 
     }
