50284e2091703e435f99dabba9aff8faeaa289f3
Alec Fortin <alec.fortin@outlook.com>
2021-02-23 13:32:46 -0500

    intro to creating generic classes

--- Collections/src/Collections.java
+++ Collections/src/Collections.java
@@ -1,35 +1,13 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Collections {
     public static void main(String[] args) {
+        Item<String> item = new Item<>();
+        item.setX("hello");
 
-        Person p = new Person("email@email.com", "Fortin");
-        Position pos1 = new Position(10,10);
-        p.position = pos1;
+        Item<Person> person = new Item<>();
+        Person p = new Person("email@email.com", "hello");
+        person.setX(p);
 
-        Person q = new Person("email@email.com", "Fortin");
-        Position pos2 = new Position(10,10);
-        q.position = pos2;
-
-        HashSet<Person> people = new HashSet<Person>();
-
-        people.add(p);
-        people.add(q);
-
-        Person newPerson = new Person("email@email.com", "Fortin");
-        Position newPos = new Position(10,10);
-        q.position = newPos;
-
-        System.out.println(people.contains(p));
-
-        HashMap<String, Person> peeps = new HashMap<String, Person>();
-        peeps.put("email@email.com", newPerson);
-        System.out.println("HashMap: " + peeps.containsKey("email@email.com"));
-
-
-
-
+        Person p2 = person.getX();
     }
 }
---
+++ Collections/src/Item.java
@@ -0,0 +1,12 @@
+public class Item<T> {
+    private T x;
+
+    T getX(){
+        return x;
+    }
+
+    void setX(T x){
+        this.x = x;
+    }
+
+}
